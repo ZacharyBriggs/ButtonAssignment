e151c925840fa7dc4eeb6a470e840aa2123d5ced
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-05-15 12:56:47 -0500

    initial

---
+++ ButtonAssignment/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ButtonAssignment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void OnFormClick(object sender, EventArgs args)
+        {
+            //If cursur is on button do stuff
+            if (button.CheckClick())
+            {
+                textBox1.Text = MousePosition.ToString();
+            }
+            else
+            {
+                textBox1.Text = "Button not clicked.";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button = new Button(300,300,150);
+        }
+    }
+}
---
+++ ButtonAssignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ButtonAssignment
+{
+    public class Button
+    {
+        public Point pos;
+        public int Area;
+
+        public delegate void TMouseClicked();
+        public event EventHandler Click;
+
+        public Button(int x, int y, int area)
+        {
+            pos.X = x;
+            pos.Y = y;
+            Area = area;
+        }
+        public bool CheckClick()
+        {
+            if (Cursor.Position.X > pos.X - Area / 2 && Cursor.Position.X < pos.X + Area / 2 &&
+                Cursor.Position.Y > pos.Y - Area / 2 && Cursor.Position.Y < pos.Y + Area / 2)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
